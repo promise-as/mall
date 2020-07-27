3b48c6c111e09e506773340149f3165db8fce352
Hong Baojin <13809735212.163.com>
2020-07-27 17:56:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  mounted() {
+    let url =
+      "https://www.easy-mock.com/mock/5f1e8ecc8de7bf79f0a62133/example/mock";
+    axios.get(url).then(() => {})
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
